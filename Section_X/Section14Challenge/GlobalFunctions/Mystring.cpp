3f0a8b57800c161ffe3cd8143005b7f67d1dc8c1
Jkaplan1123 <62909331+Jkaplan1123@users.noreply.github.com>
2023-05-18 13:25:40 -0700

    Overloaded Operator Challenge - Global Functions

--- Section_X/Section14Challenge/GlobalFunctions/Mystring.cpp
+++ Section_X/Section14Challenge/GlobalFunctions/Mystring.cpp
@@ -90,3 +90,69 @@ std::istream &operator>>(std::istream &in, Mystring &rhs) {
     return in;
 }
 
+// String Manipulation and Concatenation
+
+// unary minus - returns lowercase version of object's string
+Mystring operator-(const Mystring &rhs){
+    char *buff = new char[std::strlen(rhs.str) + 1];
+    std::strcpy(buff, rhs.str);
+    for (size_t idx = 0; idx < std::strlen(buff); idx++){
+        buff[idx] = tolower(buff[idx]);
+    }
+
+    Mystring temp {buff};
+    delete [] buff;
+    return temp;
+} 
+
+//concatenation - returns an object that concatenates the lhs and rhs
+Mystring operator+(const Mystring &lhs, const Mystring &rhs){
+    Mystring temp {std::strcat(lhs.str, rhs.str)};
+    return temp;
+}
+
+//concatenate the rhs string to the lhs string and store the result in lhs object. a += b is equivalent to a = a+b
+Mystring &operator+=(Mystring &lhs, const Mystring &rhs){
+    lhs = lhs + rhs;
+    return lhs;
+}     
+
+//create a string consisting of the initial string repeated n many times (e.g. "abc" * 3 = "abcabcabc")
+Mystring operator*(const Mystring &rhs, int n){
+    
+    Mystring temp {rhs};
+    for (size_t idx = 1; idx < n; idx++){
+        temp += rhs;
+    }
+
+    return temp;
+}
+
+//do the same as the * operator but assign it to the lhs object
+Mystring &operator*=(Mystring &lhs, int n){
+    lhs = lhs * n;
+    return lhs;
+}
+
+
+// Comparison Operator Overloading
+
+//returns true if two strings are equal
+bool operator==(Mystring &lhs, const Mystring &rhs){
+    return (std::strcmp(lhs.str, rhs.str) == 0);
+}
+
+//returns false if two strings are not equal
+bool operator!=(Mystring &lhs, const Mystring &rhs){
+    return (std::strcmp(lhs.str, rhs.str) != 0);
+}
+
+//returns true if the lhs string is lexically less than the rhs string
+bool operator<(Mystring &lhs, const Mystring &rhs){
+    return (std::strcmp(lhs.str, rhs.str) < 0);
+} 
+
+// returns false if the lhs string is lexically less than the rhs string
+bool operator>(Mystring &lhs, const Mystring &rhs){
+    return (std::strcmp(lhs.str, rhs.str) > 0);
+} 
--- Section_X/Section14Challenge/GlobalFunctions/Mystring.h
+++ Section_X/Section14Challenge/GlobalFunctions/Mystring.h
@@ -6,6 +6,20 @@ class Mystring
     friend std::ostream &operator<<(std::ostream &os, const Mystring &rhs);
     friend std::istream &operator>>(std::istream &in, Mystring &rhs);
 
+    // String Manipulation and Concatenation
+    friend Mystring operator-(const Mystring &rhs); // unary minus - returns lowercase version of object's string
+    friend Mystring operator+(const Mystring &lhs, const Mystring &rhs); //concatenation - returns an object that concatenates the lhs and rhs
+    friend Mystring &operator+=(Mystring &lhs, const Mystring &rhs);     //concatenate the rhs string to the lhs string and store the result in lhs object. a += b is equivalent to a = a+b
+    friend Mystring operator*(const Mystring &rhs, int n); //create a string consisting of the initial string repeated n many times (e.g. "abc" * 3 = "abcabcabc")
+    friend Mystring &operator*=(Mystring &lhs, int n); //do the same as the * operator but assign it to the lhs object
+
+
+    // Comparison Operator Overloading
+    friend bool operator==(Mystring &lhs, const Mystring &rhs); //returns true if two strings are equal
+    friend bool operator!=(Mystring &lhs, const Mystring &rhs); //returns false if two strings are not equal
+    friend bool operator<(Mystring &lhs, const Mystring &rhs); //returns true if the lhs string is lexically less than the rhs string
+    friend bool operator>(Mystring &lhs, const Mystring &rhs); // returns false if the lhs string is lexically less than the rhs string
+
 private:
     char *str;      // pointer to a char[] that holds a C-style string
 public:
