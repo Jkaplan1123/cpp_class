cdb3434a23427bdf14f4144677461dd8b7b223eb
Jkaplan1123 <62909331+Jkaplan1123@users.noreply.github.com>
2023-05-24 18:43:59 -0700

    cleaning up code

--- Section_X/Section16Challenge/Savings_Account.cpp
+++ Section_X/Section16Challenge/Savings_Account.cpp
@@ -20,5 +20,5 @@ bool Savings_Account::withdraw(double amount){
 void Savings_Account::print(std::ostream &os) const {
     os.precision(2);
     os << std::fixed;
-    os << "[Savings Account: " << name << ": $" << balance << ", interest rate:" << int_rate << "%]";
+    os << "[Savings Account: " << name << ": $" << balance << ", interest rate: " << int_rate << "%]";
 }
