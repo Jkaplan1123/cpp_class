25329765320c0afa3950c445bc119c567530edb3
Jkaplan1123 <62909331+Jkaplan1123@users.noreply.github.com>
2023-06-22 10:24:14 -0700

    Implemented IllegalBalanceException

--- Section_X/Section18Challenge/IllegalBalanceException.h
+++ Section_X/Section18Challenge/IllegalBalanceException.h
@@ -1,11 +1,18 @@
 #ifndef __ILLEGAL_BALANCE_EXCEPTION_H__
 #define __ILLEGAL_BALANCE_EXCEPTION_H__
+// #include <exception>
 
-class IllegalBalanceException
+class IllegalBalanceException : public std::exception
 {
 public:
-    IllegalBalanceException() {}
-    ~IllegalBalanceException() {}
+    IllegalBalanceException() noexcept = default;
+    ~IllegalBalanceException() = default;
+
+    // virtual const char *what() const noexcept override;
+
+    virtual const char *what() const noexcept override{
+        return "Illegal balance exception";
+    };
 };
 
 #endif // __ILLEGAL_BALANCE_EXCEPTION_H__
--- Section_X/Section18Challenge/main.cpp
+++ Section_X/Section18Challenge/main.cpp
@@ -8,8 +8,45 @@
 
 using namespace std;
 
+void illegalbalancetext(std::string act_name){
+    cerr << "Account \"" << act_name << "\" could not be created due to an illegal balance." << endl;
+}
+
 int main() {
     // test your code here
+
+    // Test IllegalBalanceException
+    {
+        // Test Illegal Balance with checking account
+        try{
+            double balance {};
+            cout << "Insert checking account starting balance: ";
+            cin >> balance;
+
+            Checking_Account checking {"Checking", balance};
+            cout << "\nAccount checking successfully created" << endl;
+            cout << checking << endl << endl;
+        }
+        catch (IllegalBalanceException){
+            illegalbalancetext("checking");
+        }
+
+        // Test Illegal Balance with savings account
+
+        try{
+            double balance {};
+            cout << "Insert savings account starting balance: ";
+            cin >> balance;
+
+            Savings_Account savings {"Savings", balance};
+            cout << "\nAccount Savings successfully created" << endl;
+            cout << savings << endl << endl;
+        }
+        catch (IllegalBalanceException){
+            illegalbalancetext("Savings");
+        }
+    }
+
     std::cout << "Program completed successfully" << std::endl;
     return 0;
 }
