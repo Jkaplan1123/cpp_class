3e0f29f3ed162e765daf83ae11a92038327a3317
Jkaplan1123 <62909331+Jkaplan1123@users.noreply.github.com>
2023-07-13 18:33:43 -0700

    Cleaning up

--- Section_4/FirstProgram/FirstProgram/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-int main(){
-    int favorite_number;
-    std::cout << "Enter your favorite number between 1 and 100: ";
-    std::cin >> favorite_number; //Read user input from console and store as favorite_number
-    std::cout << "Me too!" << std::endl;
-    return 0;
-}
